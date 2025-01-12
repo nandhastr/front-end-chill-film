a75ebe2a2e0af88d4df656b46a28a438e85d71e7
nandhastr <nandhastr2@gmail.com>
2025-01-12 12:14:59 +0700

    fix respoonsive pop up film

--- src/pages/LanggananPaket.jsx
+++ src/pages/LanggananPaket.jsx
@@ -38,11 +38,11 @@ const LanggananPaket = () => {
           <section>
               <div className=" text-white text-center py-10">
                   <h2 className="text-2xl font-semibold mb-8 px-8 md:px-10">Kenapa Harus Berlangganan?</h2>
-                  <div className=" mx-[10rem]">
-                      <div className="grid grid-cols-1 md:grid-cols-3  gap-12">
+                  <div className=" mx-[2rem] justify-center items-center">
+                      <div className="grid grid-cols-2 sm:grid-cols-2  md:grid-cols-3  md:gap-12 gap-20">
                           {benefits.map((benefit, index) => (
-                              <div key={index} className="flex flex-col items-center justify-center text-center">
-                                  <img src={benefit.icon} alt={benefit.title} className="md:w-10 max-w-10  mb-4" />
+                              <div key={index} className="flex flex-col  items-center justify-center text-center">
+                                  <img src={benefit.icon} alt={benefit.title} className="md:max-w-10 max-w-8  mb-4" />
                                   <p className="text-xs md:text-sm font-medium">{benefit.title}</p>
                               </div>
                           ))}
@@ -50,7 +50,7 @@ const LanggananPaket = () => {
                   </div>
               </div>
           </section>
-          <section className="bg-[#22282A] w-full py-10">
+          <section className="bg-[#22282A] w-[100%] py-10">
               <PaketChoice />
           </section>
           <section>
