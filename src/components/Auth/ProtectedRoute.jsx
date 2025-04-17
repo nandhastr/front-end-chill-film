691dfd87160d3306c5a61bf551eca52ea842ef5b
nandhastr <nandhastr2@gmail.com>
2025-04-17 09:57:00 +0700

    fix logout

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
-    console.log("isLoggedIn:", isLoggedIn); // apakah "true" atau null/false
 
     return isLoggedIn === "true" ? children : <Navigate to="/" />;
 };
